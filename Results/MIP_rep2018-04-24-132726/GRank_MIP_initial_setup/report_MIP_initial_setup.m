a7afb7ac157f354218d201ab6b062b104768db82
ZGeorgeH <george-92@live.cn>
2018-04-24 15:42:39 +0100

    Sensitivity tests, LRank and GRank

---
+++ Results/MIP_rep2018-04-24-132726/GRank_MIP_initial_setup/report_MIP_initial_setup.m
@@ -0,0 +1,19 @@
+   ***********************************
+   *    AMIGO2, Copyright @CSIC      *
+   *    AMIGO2_R2017a [March 2017]    *
+   *********************************** 
+
+Date: 24-Apr-2018
+Problem folder:	 Results\MIP_rep2018-04-24-132726
+Results folder in problem folder:	 Results\MIP_rep2018-04-24-132726\GRank_MIP_initial_setup 
+
+
+---------------------------------------------------
+Local sensitivity problem related active settings
+---------------------------------------------------
+senssolver: cvodes
+ivp_RelTol: 1e-05
+ivp_AbsTol: 1e-07
+sensmex: cvodesg_MIP
+MaxStepSize: Inf
+MaxNumberOfSteps: 1e+06
