4dbd546fb4cb0d9fe19fb8064f7f5a2d132f54e5
ZGeorgeH <george-92@live.cn>
2018-04-25 16:32:49 +0100

    ! debugging: these two lines may cause error when running PE

--- Preprocessor/AMIGO_gen_obs.m
+++ Preprocessor/AMIGO_gen_obs.m
@@ -101,7 +101,7 @@ if inputs.pathd.force_gen_obs
     rehash;
     
     if inputs.exps.NLObs
-        flag = AMIGO_gen_obs_sens(inputs,results);
+        flag = AMIGO_gen_obs_sens(inputs);
         rehash;
     end
 end
