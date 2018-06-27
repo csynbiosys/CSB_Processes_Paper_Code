02822acc995d069bbac450b0efb3c0257417ba78
ZGeorgeH <george-92@live.cn>
2018-06-27 09:35:31 +0100

    Implement the new Highest Probability Density interval. and the plots

---
+++ Examples/George/credible intervals/InputComparison/credibleInterval6.m
@@ -0,0 +1,93 @@
+function [average,lb,ub,drec] = credibleInterval6...
+    (data,alpha,varargin)
+%generate the credible interval (Highest Posterior Density Region)based on
+%the given data and alpha value. The last parameter is the name of the
+%figure file, if the user want to have a record of this.
+%HDI with refine loop
+
+mind=min(data);
+maxd=max(data);
+numInterval=1000;
+dt=min(2*iqr(data)*length(data)^(-1/3),(maxd-mind)/20);
+d2=(0.75*length(data))^(-1/5);
+%count with 1000 intervals
+x=linspace(mind,maxd,numInterval+1);
+d=x(2)-x(1);
+drec=nan(20,1);
+ratio=3/(sqrt(125)*length(data));
+for count=1:20
+    p=ksdensity(data,x,'Bandwidth',d2,...
+        'Kernel','epanechnikov',...
+        'Support','positive',...
+        'BoundaryCorrection','reflection');
+    p=[0,p/sum(p),0];
+    M=sum(diff(p,2).^2)/d^3;
+    drec(count)=d2;
+    d2=(ratio*M)^(1/5);
+    if abs(drec(count)-d2)<(d/100)
+        break;
+    end
+end
+
+if (maxd-mind)/d2<20
+    d2=min(min(drec(1)),dt);
+end
+p=ksdensity(data,x,'Bandwidth',d2,...
+    'Kernel','epanechnikov',...
+    'Support','positive',...
+    'BoundaryCorrection','reflection');
+p=p/sum(p);
+cp=cumsum(p);
+a=[0,cp(cp<=alpha)];
+b=a;
+b(1)=sum(cp<=1-alpha)-1;
+for inde=2:length(a)
+    b(inde)=sum(cp-a(inde)<=1-alpha)-inde;
+    b(inde)=b(inde)+...
+        abs(p(inde)-p(b(inde)+inde));
+end
+[~,inde]=sort(b);
+
+[~,indeRev]=sort(fliplr(b));
+if (indeRev(1)==1)
+    lb=x(length(a));
+    ub=x(end);
+else
+    lb=x(inde(1));
+    ub=x(floor(b(inde(1))+inde(1)));
+end
+
+average=mean(data);
+
+%draw the figure and save it if the user provide a file name.
+if ~isempty(varargin)
+    figure();
+    histogram(data,mind:(2*d2):maxd);
+    hold on;
+    data2=data((data>=lb)&(data<=ub));
+    histogram(data2,mind:(2*d2):maxd);
+    plot(x,p*length(data)/d*2*d2);
+    ax=gca;
+    ax.XLim=[mind,maxd];
+    plot([average,average],ax.YLim,'r-','LineWidth',2);
+    plot([lb,lb],ax.YLim,'m-','LineWidth',1);
+    plot([ub,ub],ax.YLim,'m-','LineWidth',1);
+    try
+        title({'Credible Interval (Highest Probability Density Interval)';
+            ['(with Alpha=',num2str(alpha),')'];varargin{1}(1:11)});
+    catch
+        title({'Credible Interval (Highest Probability Density Interval)';
+            ['(with Alpha=',num2str(alpha),')'];varargin{1}});
+    end
+    legend('Histrogram','Values in the HPDR',...
+        'Kernel Density Estimation (rescaled)',...
+        ['Mean = ',num2str(average,'%10.3e')],...
+        ['Lower Boundary = ',num2str(lb,'%10.3e')],...
+        ['Upper Boundary = ',num2str(ub,'%10.3e')]);
+    xlabel('Parameter Values');
+    ylabel('Frequency');
+    set(ax,'FontSize',14);
+    cosavefig(varargin{1});
+    hold off;
+end
+end
---
+++ Examples/George/credible intervals/InputComparison/plotcredibleInterval3.m
@@ -0,0 +1,44 @@
+%list of the data to analyse
+filelist={'Step.mat','Pulse.mat','Random.mat'};
+%alpha level
+alpha=0.1;
+
+%set up the boundary, only for plotting
+%global_theta_max = [0.4950,0.4950,4.9,10,0.23,6.8067,0.2449,0.0217];
+%global_theta_min = [3.88e-5,3.88e-2,0.5,2,7.7e-3,0.2433,5.98e-5,0.012];
+% clear global_theta_max global_theta_min %uncomment to disable the boundary
+
+% load the parameter names and the true values
+InduciblePromoter_load_model_optimised;
+parNames=model.par_names;
+trueValues=model.par;
+clear model;
+
+for file=filelist
+    %load the file
+    load(file{1});
+    
+    %number of PE for each trial
+    t=size(fullPEresults,2);
+    %number of parameters
+    p=length(parameters);
+    
+    %set the default values to NaN matrices as pre-allocation, and any
+    %errors in the data could be clearly seen (comparing to overwriting or
+    %initializing as zeros).
+    param.mean=nan(p,1);
+    param.min=param.mean;
+    param.max=param.mean;
+    
+    for paramIndex=1:p
+            %call the function and save the mean and the min, max, of the
+            %region
+            [param.mean(paramIndex),param.min(paramIndex),...
+                param.max(paramIndex)]...
+                =credibleInterval6(parameters{paramIndex}(:,t),alpha,...
+                [file{1}(1:end-4),'-',parNames(paramIndex,:),...
+                '(alpha=',num2str(alpha),', HPDI).fig']);
+    end
+    %save the data
+    save([file{1}(1:end-4),'HPDI alpha=',num2str(alpha),'.mat'],'param');
+end
